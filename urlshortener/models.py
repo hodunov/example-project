7ec1d967a54663559b12dad60dc060513e1114e3
Artem Hodunov <hodunov.art@gmail.com>
2021-11-13 15:36:17 +0200

    Add Shortener save method

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from urlshortener.utils import create_shortened_url
+
 """
 Url shortener model
 """
@@ -26,3 +28,9 @@ class Shortener(models.Model):
     def __str__(self):
         return f"{self.long_url} to {self.short_url}"
 
+    def save(self, *args, **kwargs):
+
+        if not self.short_url:
+            self.short_url = create_shortened_url(self)
+
+        super().save(*args, **kwargs)
